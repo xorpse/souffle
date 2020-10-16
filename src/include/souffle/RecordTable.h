87857d355fe8f7c19beb51ff25ed94b8c0807944
Olivier Hamel <ohamel@reshiftsecurity.com>
2020-10-16 16:39:16 -0400

    avoid a copy

--- src/include/souffle/RecordTable.h
+++ src/include/souffle/RecordTable.h
@@ -92,7 +92,7 @@ public:
         for (size_t i = 0; i < arity; i++) {
             tmp[i] = tuple[i];
         }
-        return pack(tmp);
+        return pack(std::move(tmp));
     }
 
     /** @brief convert record reference to a record pointer */
