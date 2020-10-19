5b401e17cadb0a31b881eaaa944d84933fd7d52e
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-10-19 13:37:26 +1100

    Moved over constraint-translator functionality into cpp file.

--- src/ast2ram/ConstraintTranslator.cpp
+++ src/ast2ram/ConstraintTranslator.cpp
@@ -8,6 +8,80 @@
 
 /************************************************************************
  *
- * @file ConstraintTranslator.h
+ * @file ConstraintTranslator.cpp
  *
  ***********************************************************************/
+
+#include "ast2ram/ConstraintTranslator.h"
+#include "ast/Atom.h"
+#include "ast/BinaryConstraint.h"
+#include "ast/TranslationUnit.h"
+#include "ast/analysis/AuxArity.h"
+#include "ast2ram/AstToRamTranslator.h"
+#include "ast2ram/ValueIndex.h"
+#include "ram/Constraint.h"
+#include "ram/Expression.h"
+#include "ram/Negation.h"
+#include "ram/ProvenanceExistenceCheck.h"
+#include "ram/UndefValue.h"
+
+namespace souffle::ast2ram {
+
+Own<ram::Condition> ConstraintTranslator::visitAtom(const ast::Atom&) {
+    return nullptr;  // covered already within the scan/lookup generation step
+}
+
+Own<ram::Condition> ConstraintTranslator::visitBinaryConstraint(const ast::BinaryConstraint& binRel) {
+    auto valLHS = translator.translateValue(binRel.getLHS(), index);
+    auto valRHS = translator.translateValue(binRel.getRHS(), index);
+    return mk<ram::Constraint>(binRel.getOperator(), std::move(valLHS), std::move(valRHS));
+}
+
+Own<ram::Condition> ConstraintTranslator::visitProvenanceNegation(const ast::ProvenanceNegation& neg) {
+    const auto* atom = neg.getAtom();
+    size_t auxiliaryArity = translator.getEvaluationArity(atom);
+    assert(auxiliaryArity <= atom->getArity() && "auxiliary arity out of bounds");
+    size_t arity = atom->getArity() - auxiliaryArity;
+    VecOwn<ram::Expression> values;
+
+    auto args = atom->getArguments();
+    for (size_t i = 0; i < arity; i++) {
+        values.push_back(translator.translateValue(args[i], index));
+    }
+    // we don't care about the provenance columns when doing the existence check
+    if (Global::config().has("provenance")) {
+        // undefined value for rule number
+        values.push_back(mk<ram::UndefValue>());
+        // add the height annotation for provenanceNotExists
+        for (size_t height = 1; height < auxiliaryArity; height++) {
+            values.push_back(translator.translateValue(args[arity + height], index));
+        }
+    }
+    return mk<ram::Negation>(
+            mk<ram::ProvenanceExistenceCheck>(translator.getConcreteRelationName(atom), std::move(values)));
+}
+
+Own<ram::Condition> ConstraintTranslator::visitNegation(const ast::Negation& neg) {
+    const auto* atom = neg.getAtom();
+    size_t auxiliaryArity = translator.getEvaluationArity(atom);
+    assert(auxiliaryArity <= atom->getArity() && "auxiliary arity out of bounds");
+    size_t arity = atom->getArity() - auxiliaryArity;
+
+    if (arity == 0) {
+        // for a nullary, negation is a simple emptiness check
+        return mk<ram::EmptinessCheck>(translator.getConcreteRelationName(atom));
+    }
+
+    // else, we construct the atom and create a negation
+    VecOwn<ram::Expression> values;
+    auto args = atom->getArguments();
+    for (size_t i = 0; i < arity; i++) {
+        values.push_back(translator.translateValue(args[i], index));
+    }
+    for (size_t i = 0; i < auxiliaryArity; i++) {
+        values.push_back(mk<ram::UndefValue>());
+    }
+    return mk<ram::Negation>(
+            mk<ram::ExistenceCheck>(translator.getConcreteRelationName(atom), std::move(values)));
+}
+}  // namespace souffle::ast2ram
--- src/ast2ram/ConstraintTranslator.h
+++ src/ast2ram/ConstraintTranslator.h
@@ -14,10 +14,7 @@
 
 #pragma once
 
-#include "ast/BinaryConstraint.h"
 #include "ast/utility/Visitor.h"
-#include "ast2ram/AstToRamTranslator.h"
-#include "ast2ram/ValueIndex.h"
 #include "ram/Call.h"
 #include "ram/Clear.h"
 #include "ram/Condition.h"
@@ -58,75 +55,32 @@
 #include "ram/utility/Utils.h"
 #include "souffle/utility/ContainerUtil.h"
 
-namespace souffle::ast2ram {
-class ConstraintTranslator : public ast::Visitor<Own<ram::Condition>> {
-    AstToRamTranslator& translator;
-    const ValueIndex& index;
+namespace souffle::ast {
+class Atom;
+class BinaryConstraint;
+class Negation;
+class ProvenanceNegation;
+}  // namespace souffle::ast
 
+namespace souffle::ast2ram {
+
+class ValueIndex;
+class AstToRamTranslator;
+
+class ConstraintTranslator : public ast::Visitor<Own<ram::Condition>> {
 public:
     ConstraintTranslator(AstToRamTranslator& translator, const ValueIndex& index)
             : translator(translator), index(index) {}
 
-    /** for atoms */
-    Own<ram::Condition> visitAtom(const ast::Atom&) override {
-        return nullptr;  // covered already within the scan/lookup generation step
-    }
+    /** -- Visitors -- */
+    Own<ram::Condition> visitAtom(const ast::Atom&) override;
+    Own<ram::Condition> visitBinaryConstraint(const ast::BinaryConstraint& binRel) override;
+    Own<ram::Condition> visitProvenanceNegation(const ast::ProvenanceNegation& neg) override;
+    Own<ram::Condition> visitNegation(const ast::Negation& neg) override;
 
-    /** for binary relations */
-    Own<ram::Condition> visitBinaryConstraint(const ast::BinaryConstraint& binRel) override {
-        auto valLHS = translator.translateValue(binRel.getLHS(), index);
-        auto valRHS = translator.translateValue(binRel.getRHS(), index);
-        return mk<ram::Constraint>(binRel.getOperator(), std::move(valLHS), std::move(valRHS));
-    }
-
-    /** for provenance negation */
-    Own<ram::Condition> visitProvenanceNegation(const ast::ProvenanceNegation& neg) override {
-        const auto* atom = neg.getAtom();
-        size_t auxiliaryArity = translator.getEvaluationArity(atom);
-        assert(auxiliaryArity <= atom->getArity() && "auxiliary arity out of bounds");
-        size_t arity = atom->getArity() - auxiliaryArity;
-        VecOwn<ram::Expression> values;
-
-        auto args = atom->getArguments();
-        for (size_t i = 0; i < arity; i++) {
-            values.push_back(translator.translateValue(args[i], index));
-        }
-        // we don't care about the provenance columns when doing the existence check
-        if (Global::config().has("provenance")) {
-            // undefined value for rule number
-            values.push_back(mk<ram::UndefValue>());
-            // add the height annotation for provenanceNotExists
-            for (size_t height = 1; height < auxiliaryArity; height++) {
-                values.push_back(translator.translateValue(args[arity + height], index));
-            }
-        }
-        return mk<ram::Negation>(mk<ram::ProvenanceExistenceCheck>(
-                translator.getConcreteRelationName(atom), std::move(values)));
-    }
-
-    /** for negations */
-    Own<ram::Condition> visitNegation(const ast::Negation& neg) override {
-        const auto* atom = neg.getAtom();
-        size_t auxiliaryArity = translator.getEvaluationArity(atom);
-        assert(auxiliaryArity <= atom->getArity() && "auxiliary arity out of bounds");
-        size_t arity = atom->getArity() - auxiliaryArity;
-
-        if (arity == 0) {
-            // for a nullary, negation is a simple emptiness check
-            return mk<ram::EmptinessCheck>(translator.getConcreteRelationName(atom));
-        }
-
-        // else, we construct the atom and create a negation
-        VecOwn<ram::Expression> values;
-        auto args = atom->getArguments();
-        for (size_t i = 0; i < arity; i++) {
-            values.push_back(translator.translateValue(args[i], index));
-        }
-        for (size_t i = 0; i < auxiliaryArity; i++) {
-            values.push_back(mk<ram::UndefValue>());
-        }
-        return mk<ram::Negation>(
-                mk<ram::ExistenceCheck>(translator.getConcreteRelationName(atom), std::move(values)));
-    }
+private:
+    AstToRamTranslator& translator;
+    const ValueIndex& index;
 };
+
 }  // namespace souffle::ast2ram
