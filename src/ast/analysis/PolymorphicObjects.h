abe6d44742aeddc5860fa9b2a296e9ece0a288ef
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-10-20 17:44:38 +1100

    Set up interface foundations for PO analysis.

---
+++ src/ast/analysis/PolymorphicObjects.h
@@ -0,0 +1,49 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2020, The Souffle Developers. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file PolymorphicObjects.h
+ *
+ ***********************************************************************/
+
+#pragma once
+
+#include "ast/NumericConstant.h"
+#include "ast/analysis/Analysis.h"
+#include <map>
+
+namespace souffle {
+enum class AggregateOp;
+enum class BinaryConstraintOp;
+enum class FunctorOp;
+}  // namespace souffle
+
+namespace souffle::ast {
+class Aggregator;
+class BinaryConstraint;
+class IntrinsicFunctor;
+}  // namespace souffle::ast
+
+namespace souffle::ast::analysis {
+
+class PolymorphicObjectsAnalysis : Analysis {
+public:
+    FunctorOp getFunctionOp(const IntrinsicFunctor*) const;
+    NumericConstant::Type getType(const NumericConstant*) const;
+    BinaryConstraintOp getOperator(const BinaryConstraint*) const;
+    AggregateOp getOperator(const Aggregator*) const;
+
+private:
+    std::map<const IntrinsicFunctor*, FunctorOp> functorType;
+    std::map<const NumericConstant*, NumericConstant::Type> constantType;
+    std::map<const BinaryConstraint*, BinaryConstraintOp> constraintType;
+    std::map<const Aggregator*, AggregateOp> aggregatorType;
+};
+
+}  // namespace souffle::ast::analysis
