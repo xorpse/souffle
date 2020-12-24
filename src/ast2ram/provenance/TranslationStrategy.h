f11fd2e6424ecd39b167dda7c0ed3f2e8517082a
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-12-24 13:31:35 +1100

    Added names for evaluation strategies.

--- src/ast2ram/provenance/TranslationStrategy.h
+++ src/ast2ram/provenance/TranslationStrategy.h
@@ -36,6 +36,10 @@ namespace souffle::ast2ram::provenance {
 
 class TranslationStrategy : public ast2ram::TranslationStrategy {
 public:
+    std::string getName() const override {
+        return "ProvenanceEvaluation";
+    }
+
     ast2ram::UnitTranslator* createUnitTranslator() const override;
     ast2ram::ClauseTranslator* createClauseTranslator(
             const TranslatorContext& context, SymbolTable& symbolTable) const override;
