91b2c0e64de480623e5d25219cd37a0273f39510
Sam Arch <33534453+SamArch27@users.noreply.github.com>
2021-06-07 18:32:35 +1000

    Update functors.cpp

--- tests/interface/graph_coloring/functors.cpp
+++ tests/interface/graph_coloring/functors.cpp
@@ -9,7 +9,6 @@ uint32_t log_wrapper(uint32_t n) {
 }
 
 uint32_t get_rightmost_unset_bit(uint32_t n) {
-
     for (uint32_t index = 0; index < 32; ++index) {
         if ((n & (1 << index)) == 0) {
             return 1 << index;
