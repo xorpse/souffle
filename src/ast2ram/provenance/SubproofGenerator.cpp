059241f64d39409856df28075bcc167a18a887b1
Olivier Hamel <ohamel@reshiftsecurity.com>
2021-08-20 13:53:39 -0400

    nfc tactfulise

--- src/ast2ram/provenance/SubproofGenerator.cpp
+++ src/ast2ram/provenance/SubproofGenerator.cpp
@@ -130,7 +130,7 @@ Own<ram::Operation> SubproofGenerator::addBodyLiteralConstraints(
                     BinaryConstraintOp::EQ, std::move(lhs), mk<ram::SubroutineArgument>(i));
             op = mk<ram::Filter>(std::move(constraint), std::move(op));
         } else if ([[maybe_unused]] const auto* adt = as<ast::BranchInit>(arg)) {
-            (void)adt;  // GCC 9.2.1 is stupid and ugly and isn't honouring `[[maybe_unused]]`
+            (void)adt;  // GCC 9.2.1 isn't honouring `[[maybe_unused]]` in a conditional
             // TODO (azreika): fill this out like record arguments
             assert(false && adt && "unhandled");
         }
@@ -233,7 +233,7 @@ Own<ram::Operation> SubproofGenerator::generateReturnInstantiatedValues(const as
             values.push_back(context.translateValue(*valueIndex, rec));
             values.push_back(mk<ram::SubroutineArgument>(i));
         } else if ([[maybe_unused]] const auto* adt = as<ast::BranchInit>(arg)) {
-            (void)adt;  // GCC 9.2.1 is stupid and ugly and isn't honouring `[[maybe_unused]]`
+            (void)adt;  // GCC 9.2.1 isn't honouring `[[maybe_unused]]` in a conditional
             // TODO (azreika): fill this out like record arguments
             assert(false && adt && "unhandled");
         }
