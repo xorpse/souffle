07eda5aca10b99eeb6f28377c0382c639ad30768
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2021-10-27 12:35:54 +1100

    Refractoring Clause translator

--- src/ast2ram/ClauseTranslator.h
+++ src/ast2ram/ClauseTranslator.h
@@ -41,12 +41,18 @@ public:
     ClauseTranslator(const TranslatorContext& context, TranslationMode mode) : context(context), mode(mode) {}
     virtual ~ClauseTranslator() = default;
 
+    /** Translate a non-recursive clause */ 
     virtual Own<ram::Statement> translateNonRecursiveClause(const ast::Clause& clause) = 0;
+
+    /** Translate a recursive clause */
     virtual Own<ram::Statement> translateRecursiveClause(
             const ast::Clause& clause, const std::set<const ast::Relation*>& scc, std::size_t version) = 0;
 
 protected:
+    /** Translation context */ 
     const TranslatorContext& context;
+
+    /** Translation mode */ 
     enum TranslationMode mode;
 };
 
