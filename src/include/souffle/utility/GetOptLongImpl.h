3bd42ca40d1714f11858cf35188baf56fa29db36
Quentin Sabah <quentin.sabah@gmail.com>
2022-01-25 15:19:13 +0100

    copyright notices

--- src/include/souffle/utility/GetOptLongImpl.h
+++ src/include/souffle/utility/GetOptLongImpl.h
@@ -1,3 +1,11 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2022, The Souffle Developers. All rights reserved
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
 // Implementation of getopt_long for Windows.
 #pragma once
 #ifdef USE_CUSTOM_GETOPTLONG
